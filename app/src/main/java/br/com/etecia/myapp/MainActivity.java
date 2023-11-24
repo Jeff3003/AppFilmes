25dca5145a381d62ae4aae35cf2642d216667605
Jeff3003 <jefferson_augusto22@hotmail.com>
2023-11-23 21:57:33 -0300

    atualiza05

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
         idRecycleViewFilmes = findViewById(R.id.idRecycleViewFilmes);
 
+        idRecycleViewFilmes.setLayoutManager();
+
 
     }
 }
